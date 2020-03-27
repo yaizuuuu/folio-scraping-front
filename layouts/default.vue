87e19c84f9c08e81970503401d69430935b1b727
yaizuuuu <p.i.o.y.m.a.a@gmail.com>
2020-03-27 21:34:35 +0900

    forntを実装

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <b-navbar type="dark" variant="dark" class="mb-5">
+      <b-navbar-nav>
+        <b-nav-item>Folio Scraping</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+
+    <b-container>
+      <b-row>
+        <b-col md="10" offset-md="1">
+          <nuxt />
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="mb-3">
+      <b-form inline>
+        <label class="sr-only" for="brand-name">Name</label>
+        <b-input
+          id="brand-name"
+          v-model="brandName"
+          class="mb-2 mr-sm-2 mb-sm-0"
+          placeholder="銘柄名: pets"
+        />
+        <b-button
+          variant="primary"
+          @click="getData"
+        >
+          Save
+        </b-button>
+      </b-form>
+    </div>
+    <b-table striped hover :items="items" />
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      brandName: '',
+      items: []
+    }
+  },
+  methods: {
+    async getData () {
+      const response = await this.$axios.$get(`http://localhost/theme/${this.brandName}`)
+      this.items = response.result
+    }
+  }
+}
+</script>
